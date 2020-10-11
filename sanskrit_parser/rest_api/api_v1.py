523daf0abfb1de9193f209464ac3e1ce8da2e922
Hareesh Polla <hpolla@atb.com>
2020-10-10 22:00:58 -0600

    api splitter and sandhi

--- sanskrit_parser/rest_api/api_v1.py
+++ sanskrit_parser/rest_api/api_v1.py
@@ -5,6 +5,7 @@ from random import randint
 import subprocess
 from os import path
 import sqlite3
+from html.parser import HTMLParser
 
 from sanskrit_parser.base.sanskrit_base import SanskritObject, SLP1
 from sanskrit_parser.parser.sandhi_analyzer import LexicalSandhiAnalyzer
@@ -24,6 +25,9 @@ api = flask_restx.Api(app=api_blueprint, version='1.0', title='sanskrit_parser A
                       prefix=URL_PREFIX, doc='/docs')
 
 dict_ns = api.namespace('dict', description='Dictionaries operations')
+
+LEXICON_DICT_LIST = ["vcp", "skd", "mw", "mwe"]
+LEXICON_SAN_DICT_LIST = ["vcp", "skd", "mw"]
 
 analyzer = LexicalSandhiAnalyzer()
 
@@ -174,20 +178,27 @@ class Morpho(Resource):
 @dict_ns.route('/<string:dictionary>/keys/<string:text>')
 @dict_ns.param('max_hits', default=10)
 @dict_ns.param('search_text', default=False)
+@dict_ns.param('in_devanagari', default=False)
 class DictKeySearch(Resource):
     def get(self, dictionary, text):
         parser = reqparse.RequestParser()
         parser.add_argument('max_hits', type=int, default=10)
         parser.add_argument('search_text', type=bool, default=False)
+        parser.add_argument('in_devanagari', type=bool, default=False)
         args = parser.parse_args()
         max_hits = args.get('max_hits', 10)
         search_text = args.get('search_text', False)
+        in_devanagari = args.get('in_devanagari', False)
 
         """ Get keys matching for text """
         # pobj = SanskritObject(text, strict_io=strict_io)
-
-        t = ('%' + text + '%', '%' + text + '%', max_hits) if search_text else ('%' + text + '%', max_hits)
+        if text.find('*') >= 0 or text.find('_') >= 0:
+            t = text.replace('*', '%')
+            t = (t, t + '%', max_hits) if search_text else (t, max_hits)
+        else:
+            t = (text + '%', '%' + text + '%', max_hits) if search_text else (text + '%', max_hits)
         keys = []
+        keys_devanagari = []
 
         try:
             if not DICT_CONN_LIST[dictionary]:
@@ -196,62 +207,70 @@ class DictKeySearch(Resource):
 
             c = DICT_CONN_LIST[dictionary].cursor()
             res = []
+
             if search_text:
-                res = c.execute(f"select distinct key from {dictionary} where key like ? or data like ? limit ?", t)
+                res = c.execute(f"select distinct key from {dictionary} where key like ? or data like ? limit ? ", t)
             else:
                 res = c.execute(f"select distinct key from {dictionary} where key like ? limit ?", t)
+
             for row in res:
-                keys.append(row[0])
+                item = {'key': row[0], 'devanagari': ''}
+                if in_devanagari and dictionary in LEXICON_SAN_DICT_LIST:
+                    vobj = SanskritObject(row[0], strict_io=strict_io, replace_ending_visarga=None)
+                    item['devanagari'] = vobj.devanagari()
+                keys.append(item)
         except Exception as e:
             print('exception', e)
             pass
         # finally:
             # vcp_dict_conn.close()
 
-        # r = {"input": text, "devanagari": pobj.devanagari(), "keys": keys}
         r = {"input": text,  "keys": keys}
         return r
 
 
 @dict_ns.route('/<string:dictionary>/meanings')
 @dict_ns.param('max_hits', default=10)
+@dict_ns.param('in_devanagari', default=False)
 class DictMeaningSearch(Resource):
     def post(self, dictionary):
         parser = reqparse.RequestParser()
         parser.add_argument('max_hits', type=int, default=10)
+        parser.add_argument('in_devanagari', type=bool, default=False)
         parser.add_argument('keys', type=list, default=[], location="json")
         args = parser.parse_args()
         max_hits = args.get('max_hits', 10)
+        in_devanagari = args.get('in_devanagari', False)
         keys = args.get('keys', [])
 
         res = []
 
         for key in keys:
-            sub_keys = get_meanings_for_key(dictionary, key, max_hits)
+            sub_keys = get_meanings_for_key(dictionary, key, in_devanagari, max_hits)
             res.extend(sub_keys)
 
-        # r = {"input": key, "devanagari": pobj.devanagari(), "keys": keys}
         r = {"input": keys,  "keys": res}
         return r
 
 
 @dict_ns.route('/<string:dictionary>/meanings/<string:key>')
 @dict_ns.param('max_hits', default=10)
+@dict_ns.param('in_devanagari', default=False)
 class DictMeaningSearch(Resource):
     def get(self, dictionary, key):
         parser = reqparse.RequestParser()
         parser.add_argument('max_hits', type=int, default=10)
+        parser.add_argument('in_devanagari', type=bool, default=False)
         args = parser.parse_args()
         max_hits = args.get('max_hits', 10)
+        in_devanagari = args.get('in_devanagari', False)
+        keys = get_meanings_for_key(dictionary, key, in_devanagari, max_hits)
 
-        keys = get_meanings_for_key(dictionary, key, max_hits)
-
-        # r = {"input": key, "devanagari": pobj.devanagari(), "keys": keys}
         r = {"input": key,  "keys": keys}
         return r
 
 
-def get_meanings_for_key(dictionary, key, max_hits):
+def get_meanings_for_key(dictionary, key, in_devanagari, max_hits):
     """ Get keys matching for key """
     # pobj = SanskritObject(key, strict_io=strict_io)
 
@@ -269,10 +288,8 @@ def get_meanings_for_key(dictionary, key, max_hits):
             res = {
                 'key': row[0],
                 'lnum': row[1],
-                'data': row[2]
+                'data': convert_content_to_markdown(dictionary=dictionary, content=row[2], in_devanagari=in_devanagari)
             }
-            # robj = SanskritObject(res['data'], strict_io=strict_io)
-            # res['dataDevanagari'] = robj.devanagari()
             keys.append(res)
     except Exception as e:
         print('exception', e)
@@ -281,3 +298,70 @@ def get_meanings_for_key(dictionary, key, max_hits):
         # DICT_CONN_LIST[dictionary].close()
 
     return keys
+
+
+def convert_content_to_markdown(dictionary, content, in_devanagari):
+    if dictionary in LEXICON_DICT_LIST:
+        return convert_lexicon_html_to_markdown(dictionary=dictionary, in_devanagari=in_devanagari, content=content)
+    return content
+
+
+def convert_lexicon_html_to_markdown(dictionary, in_devanagari, content):
+    parser = LexiconHTMLParser()
+    parser.init(dictionary=dictionary, in_devanagari=in_devanagari)
+    parser.feed(content)
+
+    return parser.mark_down
+
+# lexicon sample entry
+# <H1><h><key1>AGrARa</key1><key2>AGrARa</key2></h><body><s>AGrARa tri0 A + GrA</s>—<s>kta . 1 gfhItaganDe puzpAdO nAsi-</s> <lb/><s>kayA yasya ganDajYAnaM jAtaM tasmin 2 tfpte ca BAve kta .</s> <lb/><s>3 ganGagrahaRe 4 tfptO ca na0 .</s></body><tail><L>6432</L><pc>0624,a</pc></tail></H1>
+
+
+class LexiconHTMLParser(HTMLParser):
+    tag_stack = []
+    current_tag = ''
+    mark_down = ''
+    dictionary = ''
+    in_devanagari = False
+
+    def init(self, dictionary='', in_devanagari=False):
+        self.dictionary = dictionary
+        self.in_devanagari = in_devanagari
+
+    def handle_starttag(self, tag, attrs):
+        # print("Encountered a start tag:", tag)
+        self.current_tag = tag
+        self.tag_stack.append(tag)
+        data = ''
+        if tag == 'h':
+            data = '**'
+        elif tag.startswith('key'):
+            data = ' _'
+        elif tag in ['body', 'lb', 's']:
+            data = '  \n'
+        else:
+            data = ''
+
+        self.mark_down = self.mark_down + data
+
+    def handle_endtag(self, tag):
+        # print("Encountered an end tag :", tag)
+        self.current_tag = self.tag_stack.pop()
+        data = ''
+        if tag == 'h':
+            data = '**'
+        elif tag.startswith('key'):
+            data = '_ '
+        else:
+            data = ''
+        self.mark_down = self.mark_down + data
+
+    def handle_data(self, data):
+        # print("Encountered some data  :", self.current_tag, ': ', data)
+        final_data = data
+        if self.current_tag in ['l', 'pc']:  # as of not not using this info
+            return
+        if self.current_tag == 's' and self.in_devanagari:  # convert to sanskrit
+            vobj = SanskritObject(data, strict_io=strict_io, replace_ending_visarga=None)
+            final_data = vobj.devanagari()
+        self.mark_down = self.mark_down + final_data
